adc578d410a7b29b61269462812d0dd4a39d7548
sol2101 <sol2101@yandex.ru>
2019-10-13 19:22:38 +0300

    feat: implement doubly linked list data structure

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,147 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+  constructor() {
+    this._head = null;
+    this._tail = null;
+    this.length = 0;
+  }
 
-    append(data) {}
+  append(data) {
+    const newNode = new Node(data);
 
-    head() {}
+    if (!this._head) {
+      this._head = newNode;
+    } else {
+      this._tail.next = newNode;
+      newNode.prev = this._tail;
+    }
 
-    tail() {}
+    this._tail = newNode;
+    this.length++;
+    return this;
+  }
 
-    at(index) {}
+  head() {
+    return this._head == null ? null : this._head.data;
+  }
 
-    insertAt(index, data) {}
+  tail() {
+    return this._tail == null ? null : this._tail.data;
+  }
 
-    isEmpty() {}
+  at(index) {
+    let node = this._head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
 
-    clear() {}
+    return node.data;
+  }
 
-    deleteAt(index) {}
+  insertAt(index, data) {
+    let node = this._head;
 
-    reverse() {}
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
 
-    indexOf(data) {}
+    if (node == null) {
+      this.append(data);
+    } else {
+      let prev = node.prev;
+
+      let newNode = new Node(data, node.prev, node);
+      node.prev = newNode;
+
+      if (index == 0) {
+        this._head = newNode;
+      } else {
+        prev.next = newNode;
+      }
+
+      this.length++;
+    }
+  }
+
+  isEmpty() {
+    return this.length == 0;
+  }
+
+  clear() {
+    this._head = null;
+    this._tail = null;
+    this.length = 0;
+
+    return this;
+  }
+
+  deleteAt(index) {
+    let node = this._head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+
+    let prev = node.prev;
+    let next = node.next;
+
+    if (prev == null && next == null) {
+      this.clear();
+    } else if (prev == null) {
+      next.prev = null;
+    } else if (next == null) {
+      prev.next = null;
+    } else {
+      prev.next = next;
+      next.prev = prev;
+    }
+
+    this.length--;
+
+    return this;
+  }
+
+  reverse() {
+    let current = this._head;
+    let prev = null;
+    let next = null;
+
+    while (current) {
+      next = current.next;
+      prev = current.prev;
+
+      current.next = prev;
+      current.prev = next;
+
+      prev = current;
+      current = next;
+    }
+
+    this._tail = this._head;
+    this._head = prev;
+
+    return this;
+  }
+
+  indexOf(data) {
+    let node = this._head;
+    if (node.data == data) {
+      return 0;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      node = node.next;
+      if (node.data == data) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
 }
 
+
 module.exports = LinkedList;
+
+const list = new LinkedList();
+list.append(4).reverse().deleteAt(0);
